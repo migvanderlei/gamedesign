1fc91a30b111a86883071678fb1698a733daf5bd
Miguel de Oliveira <migvanderlei@gmail.com>
2021-09-19 23:45:50 -0400

    first commit

---
+++ Terrible Tweeters/Assets/Scripts/LevelController.cs
@@ -0,0 +1,37 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class LevelController : MonoBehaviour
+{
+    [SerializeField] string _nextLevelName;
+
+    Monster[] _monsters;
+    void OnEnable() {
+        _monsters = FindObjectsOfType<Monster>();
+    }
+    // Update is called once per frame
+    void Update()
+    {
+        if(MonstersAreAllDead())
+        {
+            GoToNextLevel();
+        }
+    }
+
+    bool MonstersAreAllDead()
+    {
+        foreach(Monster monster in _monsters)
+        {
+            if(monster.gameObject.activeSelf)
+                return false;
+        }
+        return true;            
+    }
+
+    void GoToNextLevel()
+    {
+        SceneManager.LoadScene(_nextLevelName);
+    }
+}
